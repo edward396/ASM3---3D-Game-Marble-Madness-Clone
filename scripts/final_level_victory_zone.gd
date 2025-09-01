054d2c99a47c75f6c237e9450bbe315720c615c6
Toan Nguyen <s3979056@rmit.edu.vn>
2025-09-01 13:57:20 +0700

    add YouWin sound

--- scripts/final_level_victory_zone.gd
+++ scripts/final_level_victory_zone.gd
@@ -1,7 +1,9 @@
 extends Area3D
 
 @onready var you_win: CanvasLayer = get_tree().current_scene.get_node("YouWin")
+@onready var you_win_sound: AudioStreamPlayer = you_win.get_node("YouWinSound")
 
 func _on_body_entered(body):
 	if body.name == "Ball" and you_win:
 		you_win.open_menu()
+		you_win_sound.play()
