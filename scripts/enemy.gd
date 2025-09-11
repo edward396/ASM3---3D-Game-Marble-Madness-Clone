b3b7514ba4eb00c274f12a71db932f0adb1c1dd9
TrucNguyen2211 <153247636+TrucNguyen2211@users.noreply.github.com>
2025-09-11 14:03:02 +0700

    adding little in map 3

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-@export var chase_speed: float = 2
-@onready var player: Node3D = $"../Ball"  # Gán node player (ball)
+@export var chase_speed: float = 5
+@export var repel_force: float = 50.0
+@onready var player: Node3D = $"../Ball"  
 @onready var chase_area: Area3D =  $Area3D2
 @onready var collision_area: Area3D = $ColliArea3D
 
@@ -9,10 +10,13 @@ var _is_chasing: bool = false
 var _target_position: Vector3
 
 func _ready():
+	
+	add_to_group("enemy")
+	
 	# Kết nối tín hiệu của Area3D để phát hiện khi người chơi vào khu vực
 	chase_area.body_entered.connect(_on_area_entered)
 	chase_area.body_exited.connect(_on_area_exited)
-	collision_area.body_entered.connect(_on_collision_with_player)
+	collision_area.body_entered.connect(_on_collision_with_body)
 
 func _physics_process(delta):
 	if _is_chasing:
@@ -24,15 +28,20 @@ func _physics_process(delta):
 
 func _on_area_entered(body: Node):
 	if body == player:
-		start_chasing()
+		_is_chasing = true
+		_target_position = player.global_transform.origin
 
 func _on_area_exited(body: Node):
 	if body == player:
-		stop_chasing()
+		_is_chasing = false
+		_target_position = Vector3.ZERO
 
-func _on_collision_with_player(body: Node):
-	if body == player:
-		apply_repel_force(body)
+func _on_collision_with_body(body: Node):
+	if body == player and body.has_method("repel_from"):
+		var direction = (global_transform.origin - player.global_transform.origin).normalized() 
+		var repelforce = 50.0  # Độ mạnh của lực đẩy
+		velocity = direction * repelforce
+		move_and_slide() 		
 
 func start_chasing():
 	_is_chasing = true
@@ -42,8 +51,3 @@ func stop_chasing():
 	_is_chasing = false
 	_target_position = Vector3.ZERO 
  
-func apply_repel_force(player: Node):
-	var direction = (global_transform.origin - player.global_transform.origin).normalized() 
-	var repel_force = 30.0 
-	move_and_slide() 
-	player.apply_central_impulse(-direction * repel_force)  
