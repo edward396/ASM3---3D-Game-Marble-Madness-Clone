acb1b74bc5756b995b785cfa7fb5f73c1b61aeda
John Tran <s3929597@rmit.edu.vn>
2025-09-12 10:00:57 +0700

    Add moving platforms feature
    - Create AnimatableBody3D-based moving platforms with Tween movement
    - Add multiple movement scripts for different directions (left-right, forward-back)
    - Implement relative positioning system for easy level design
    - Organize platform files in environment/platforms folder
    - Add platforms to level_02 with customizable movement patterns

---
+++ environment/platforms/animatable_body_3d2.gd
@@ -0,0 +1,19 @@
+extends AnimatableBody3D
+
+@export var move_distance: Vector3 = Vector3(-5, 0, 0)
+@export var move_duration: float = 4.0
+
+var start_position: Vector3
+var tween: Tween
+
+func _ready():
+	start_position = global_position
+	create_movement()
+
+func create_movement():
+	tween = create_tween()
+	tween.set_loops()
+	
+	var end_position = start_position + move_distance
+	tween.tween_property(self, "global_position", end_position, move_duration)
+	tween.tween_property(self, "global_position", start_position, move_duration)
